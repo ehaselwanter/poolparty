808e81dadc35ef9710460ff7e4fa50022cbcde26
Ari Lerner <ari.lerner@citrusbyte.com>
2008-07-19 21:19:33 -0700

    Updated gemspec for github

--- lib/poolparty/provider.rb
+++ lib/poolparty/provider.rb
@@ -17,6 +17,7 @@ module PoolParty
         requires :required_gems
         
         PoolParty::Provider.user_install_packages.each do |req|
+          puts "req: #{req}"
           self.send :requires, req.to_sym
         end
       end
